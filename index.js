2efba808d17801d1619a863972509e5b4842f879
seyi-adeleke <adetokunbo.adeleke@andela.com>
2018-04-08 14:26:27 +0100

    setup base cli

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-#!/usr/bin/env node
+const program = require('commander');
+const currentVersion = '0.0.1';
 
+
+program
+	.version(currentVersion)
+	.usage('<command> [options]')
+
+
+program
+  .command('tweet <tweet>')
+  .description('Create a new tweet')
+  .action(function(tweet, command) {
+    if(command) {
+        console.log(tweet)
+    }
+  });
+
+program.parse(process.argv);
